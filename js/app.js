a02234baa4499c893db190e4863c35afa22ace90
Mehdi Jarraya <mehdi.jarraya@esprit.tn>
2020-03-27 14:25:43 +0100

    DONE

---
+++ js/app.js
@@ -0,0 +1,9 @@
+new hoverEffect({
+    parent: document.querySelector('.distortion'),
+    intensity:0.2,
+    image1:'../images/bg_1.png',
+    image2:'../images/bg_2.png',
+    displacementImage:'../images/hoverEffect2.png',
+    // angle: Math.PI /4 default one
+})
+console.log("hover effect working");

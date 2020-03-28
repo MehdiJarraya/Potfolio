a7244084b8a37c7fa9492db904a53e07d896670e
Mehdi Jarraya <mehdi.jarraya@esprit.tn>
2020-03-28 18:21:48 +0100

    integration + js to assure validation and prevent default

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-var cards = [{ title: 'Mountain View', copy: 'Check out all of these gorgeous mountain trips with beautiful views of, you guessed it, the mountains', button: 'View Trips' }, { title: 'To The Beach', copy: 'Plan your next beach trip with these fabulous destinations', button: 'View Trips' }, { title: 'Desert Destinations', copy: 'It\'s the desert you\'ve always dreamed of', button: 'Book Now' }, { title: 'Explore The Galaxy', copy: 'Seriously, straight up, just blast off into outer space today', button: 'Book Now' }]
+const button = document.querySelector(".submit-button"),
+  stateMsg = document.querySelector(".pre-state-msg");
+
+const sendMail = function(e) {
+    console.log("e", e);
+  // prevent form form navigation
+  e.preventDefault();
+  button.classList.add("state-1", "animated");
+
+  setTimeout(finalButtonMsg, 2000);
+};
+
+const finalButtonMsg = function() {
+  button.classList.add("state-2");
+
+  setTimeout(setInitialButtonState, 2000);
+};
+
+const setInitialButtonState = function() {
+  button.classList.remove("state-1", "state-2", "animated");
+};
+
+button.addEventListener("click", updateButtonMsg);
